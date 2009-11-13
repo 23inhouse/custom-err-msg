1656797da4fb2b6ff574fcc2fba35edc72ef3539
a1067853 <a1067853@oapps001.its.adelaide.edu.au>
2009-11-13 15:13:10 +1030

    changed humanize to human_attribute_name so it works with I18n

--- lib/custom_error_message.rb
+++ lib/custom_error_message.rb
@@ -41,7 +41,7 @@ module CustomErrorMessage
     end
 
     def starts_with_humanized_column_followed_by_circumflex?(message)
-      @errors.keys.any?{|column| message.match(/^#{column.to_s.humanize} \^/)}
+      @errors.keys.any?{|column| message.match(/^#{@base.class.human_attribute_name(column)} \^/)}
     end
   end
 end
